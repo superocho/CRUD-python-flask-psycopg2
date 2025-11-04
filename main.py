d7cd865f2b88f7cc0f8224d8120ec3cc6831cb1c
Juan <david47728@gmail.com>
2025-11-04 13:54:14 -0300

    funciones

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, url_for
 import psycopg2
+from datetime import date
 
 app = Flask(__name__)
 
@@ -9,20 +10,34 @@ conn = psycopg2.connect(
     user="postgres",
     password="0434",
 )
+# pg: Insertar en tabla clientes
+def addCliente(conn, nombre, correo, ciudad, fecha_registro=None):
+    cursor = conn.cursor()
+    if fecha_registro is None:
+        fecha_registro = date.today()
+    cursor.execute(
+        "INSERT INTO Clientes(nombre, correo, ciudad, fecha_registro) VALUES (%s, %s, %s, %s)",
+        (nombre, correo, ciudad, fecha_registro),
+    )
+    conn.commit()
+    cursor.close()
 
-cursor = conn.cursor()
+# pg: Devuelve listado de todos los clientes
+def getClientes(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Clientes")
+    return cursor.fetchall()
 
-cursor.execute("SELECT * FROM Clientes")
-
-
-
+# web: Muestra todos los clientes
 @app.route('/clientes')
 def mostrarClientes():
-    clientes = cursor.fetchall()
+    clientes = getClientes(conn)
     return render_template('clientes.html', clientes=clientes)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
     
-cursor.close()
 conn.close()
