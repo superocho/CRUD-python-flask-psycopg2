55ef6b2e8fd4dd182996fecb78e76978a2dcc46c
Juan <david47728@gmail.com>
2025-11-05 14:01:47 -0300

    productos y css

--- main.py
+++ main.py
@@ -26,6 +26,35 @@ def getClientes():
     cursor.execute("SELECT * FROM Clientes")
     return cursor.fetchall()
 
+def getProductos():
+    cursor.execute("SELECT * FROM Productos")
+    return cursor.fetchall()
+
+def getProductosJOINCategorias():
+    cursor.execute(
+        "SELECT * FROM Productos P JOIN Categorias C ON P.id_categoria = C.id_categoria"
+    )
+    return cursor.fetchall()
+
+def addProducto(conn, producto, precio, stock, categoria):
+    cursor.execute(
+        "INSERT INTO Productos (nombre_producto, precio, stock, id_categoria) VALUES (%s, %s, %s, %s)",
+        (producto, precio, stock, categoria),    
+    )
+    conn.commit()
+
+def deleteProducto(conn, id):
+    cursor.execute("DELETE FROM Productos WHERE id_producto = %s", (id,))
+    conn.commit()
+
+def getCategoriaNombre(id):
+    cursor.execute("SELECT C.nombre_categoria FROM Categorias C WHERE id_categoria = id")
+    return cursor.fetchone()
+
+def getCategorias():
+    cursor.execute("SELECT * FROM Categorias")
+    return cursor.fetchall()
+
 # --------------------------------------------------------------------------------------------------------
 
 # web: Muestra todos los clientes
@@ -47,10 +76,32 @@ def addClienteWeb():
 # web: Eliminar cliente de la tabla Clientes
 @app.route('/delete/<string:id>', methods=['POST'])
 def deleteClienteWeb(id):
-    print(id)
     deleteCliente(conn, id)
-    print("SALIO")
     return redirect(url_for('mostrarClientes'))
+
+# web: Mostrar todos los productos
+@app.route('/productos')
+def mostrarProductos():
+    productosJOIN = getProductosJOINCategorias()
+    categorias = getCategorias()
+    return render_template('productos.html', categorias=categorias, productos=productosJOIN)
+
+# web: Agregar producto a la tabla Productos con los datos ingresados en la pagina
+@app.route('/productos/add', methods=['POST'])
+def addProductoWeb():
+    producto = request.form['producto']
+    precio = request.form['precio']
+    stock = request.form['stock']
+    categoria = request.form['categoria']
+    if(producto and precio and stock and categoria):
+        addProducto(conn, producto, precio, stock, categoria)
+    return redirect(url_for('mostrarProductos'))
+
+# web: Eliminar producto por id
+@app.route('/productos/delete/<string:id>', methods=['POST'])
+def deleteProductoWeb(id):
+    deleteProducto(conn, id)
+    return redirect(url_for('mostrarProductos'))
 
 if __name__ == '__main__':
     app.run(debug=True)
