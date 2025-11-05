0b8bfce3b87b03da98f81108a349377063091aee
juan <david47728@gmail.com>
2025-11-05 01:33:27 -0300

    eliminar y agregar cliente

--- database.py
+++ database.py
@@ -1,8 +1,40 @@
 import psycopg2
-
-conn = psycopg2.connect(
-    host="localhost",
-    database="tienda_demo",
-    user="postgres",
-    password="0434",
+connection = psycopg2.connect(
+    host = 'localhost',
+    database = 'tienda_demo',
+    user = 'postgres',
+    password = '0434',
+    port = 5432
 )
+
+# COMPROBACION SUBTOTAL = CANTIDAD * PRECIO
+# cursor = connection.cursor()
+# 
+# cursor.execute("""
+#     SELECT D.subtotal
+#     FROM Detalle_factura D
+# """)
+# 
+# subtotales = cursor.fetchall()
+# 
+# cursor.execute("""
+#     SELECT P.precio * D.cantidad
+#     FROM Productos P JOIN Detalle_factura D ON P.id_producto = D.id_producto;
+# """)
+# 
+# multiplicacion = cursor.fetchall()
+# 
+# if(len(multiplicacion) != len(subtotales)):
+#     print("Lenghts are not equal")
+# 
+# contador = 0
+# for i in range(len(multiplicacion)):
+#     if(multiplicacion[i] != subtotales[i]):
+#         print("NO SON IGUALES")
+#         contador += 1
+# 
+# if(contador == 0):
+#     print("Todos los valores son iguales")
+#     
+# cursor.close()
+# connection.close()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, render_template, url_for
-import psycopg2
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import date
 import database as db
 
 app = Flask(__name__)
-cursor = db.conn.cursor()
+conn = db.connection
+cursor = conn.cursor()
 
-# pg: Insertar en tabla clientes
+# pg: Insertar tupla en tabla clientes
 def addCliente(conn, nombre, correo, ciudad, fecha_registro=None):
     if fecha_registro is None:
         fecha_registro = date.today()
@@ -14,24 +14,46 @@ def addCliente(conn, nombre, correo, ciudad, fecha_registro=None):
         "INSERT INTO Clientes(nombre, correo, ciudad, fecha_registro) VALUES (%s, %s, %s, %s)",
         (nombre, correo, ciudad, fecha_registro),
     )
-    db.conn.commit()
-    cursor.close()
+    conn.commit()
+
+# pg: Eliminar tupla en tabla clientes pasando la id del cliente que queremos eliminar
+def deleteCliente(conn, id_cliente):
+    cursor.execute("DELETE FROM Clientes WHERE id_cliente = %s;", (id_cliente,))
+    conn.commit()
 
 # pg: Devuelve listado de todos los clientes
-def getClientes(conn):
+def getClientes():
     cursor.execute("SELECT * FROM Clientes")
     return cursor.fetchall()
+
+# --------------------------------------------------------------------------------------------------------
 
 # web: Muestra todos los clientes
 @app.route('/clientes')
 def mostrarClientes():
-    clientes = getClientes(db.conn)
+    clientes = getClientes()
     return render_template('clientes.html', clientes=clientes)
 
+# web: Agrega un cliente a la tabla Clientes con los datos ingresados en la pagina
+@app.route('/add', methods=['POST'])
+def addClienteWeb():
+    nombre = request.form['nombre']
+    correo = request.form['correo']
+    ciudad = request.form['ciudad']
+    if(nombre and correo and ciudad):
+        addCliente(conn, nombre, correo, ciudad)
+    return redirect(url_for('mostrarClientes'))
 
-
+# web: Eliminar cliente de la tabla Clientes
+@app.route('/delete/<string:id>', methods=['POST'])
+def deleteClienteWeb(id):
+    print(id)
+    deleteCliente(conn, id)
+    print("SALIO")
+    return redirect(url_for('mostrarClientes'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-db.conn.close()
+
+cursor.close()
+conn.close()
